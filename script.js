44d5f16f5d8b95929ae1e677acd92cdf8091f194
Squeshly-Freezed <251692768+Squeshly-Freezed@users.noreply.github.com>
2026-02-03 19:34:22 +1300

    Add working addition logic

--- script.js
+++ script.js
@@ -1,69 +1,96 @@
 
 const calculate = function() {
 
-    let currentValue = 0;
-    let secondInput = 0;
-    // let operator = "+";
-
     let expression = {
             firstInput: 0,
             operator: null,
             secondInput: 0,
         };
 
+    document.addEventListener("contextmenu", (event) => event.preventDefault());
     const buttonContainer = document.querySelector("#button-container");
     const display = document.querySelector("#display");
 
     buttonContainer.addEventListener("click", (event) => {
         
         if (event.target.classList.contains("number")) {
-            expression.firstInput = event.target.textContent;
-            showDisplay(expression.firstInput);
-            console.log(expression);
-
+            if (expression.operator) {
+                expression.secondInput = parseInt(event.target.textContent);
+                showDisplay(expression.secondInput);    
+            } else {
+                expression.firstInput = parseInt(event.target.textContent);
+                showDisplay(expression.firstInput);
+            }
         }
+
+
+
+
+        // if (!expression.operator && event.target.classList.contains("number")) {
+        //     expression.firstInput = event.target.textContent;
+        //     showDisplay(expression.firstInput);
+        //     console.log(expression);
+        // }
         if (event.target.classList.contains("operator")) {
-            expression.operator = event.target.textContent;
-            console.log(expression);
+            if (!expression.operator) {
+                expression.operator = event.target.textContent;
+            } else {
+                operate();
+                expression.secondInput = 0;
+                expression.operator = event.target.textContent;
+            }
         }
-
         if (event.target.classList.contains("clear")) {
-            clear(firstInput, secondInput, currentValue);
+            clear();
         }
-
+        // if (expression.operator && event.target.classList.contains("number")) {
+        //     // expression.firstInput = event.target.textContent;
+        //     expression.secondInput = event.target.textContent;
+        // }
+        if (expression.operator && event.target.classList.contains("equals")) {
+            operate();
+            expression.secondInput = 0;
+            expression.operator = null;
+        }
+        console.log(expression);
     });
 
 
     const showDisplay = function(response) {
+        display.textContent = "";
         display.textContent = response;
     }
 
-    const add = function(operand) {
-        return currentValue += operand;
+    const add = function() {
+        expression.firstInput = expression.firstInput + expression.secondInput;
     }
 
     const subtract = function(operand) {
-        return currentValue -= operand;
+        return firstInput -= operand;
     }
 
     const multiply = function(operand) {
-        return currentValue *= operand;
+        return firstInput *= operand;
     }
 
     const divide = function(operand) {
-        return currentValue /= operand;
+        return firstInput /= operand;
     }
 
-    const clear = function(firstInput, secondInput, currentValue) {
-        display.textContent = 0;
-        firstInput = 0;
-        secondInput = 0;
-        currentValue = 0;
+    const clear = function() {
+        expression.firstInput = 0;
+        expression.operator = null;
+        expression.secondInput = 0;
+        showDisplay(0);
     }
 
-    const operate = function(operator, firstInput, secondInput) {
-        // currentValue = 
-        showDisplay(currentValue);
+    const operate = function() {
+        switch (expression.operator) {
+            case "+":
+                add();
+                showDisplay(expression.firstInput);
+                break;
+        }
     }
 
 };
